5a6d31af86073253109dbf3f4686eecf86dbfe2f
JayasriNagrale <mycrystals@gmail.com>
2015-10-10 19:29:32 +0530

    Added styles for the popup

--- widget/layouts/side-slider/side-slider-template.php
+++ widget/layouts/side-slider/side-slider-template.php
@@ -5,8 +5,7 @@
 		<!-- popup content -->
 		<div class="yeloni-popup-content yeloni-popup-content-yesno">
 
-			<div class="yeloni-ynlayout">
-
+			
 				<!-- close button-->
 				<div class="yeloni-actual-content">
 
@@ -20,7 +19,7 @@
 							<span editable>It's time you upgrade your skills</span></div>
 						<br/><br/>
 						<div class="yline2" editable>Want to learn how to get</div>
-						<div class="yline3" editable>50000 subscribers</div>
+						<!--<div class="yline3" editable>50000 subscribers</div>-->
 						<div class="yline4" editable>for free?</div>							
 
 						<hr/>
@@ -33,11 +32,17 @@
 							<tr>
 								<td valign="top" width="50%">
 									<a ng-href="{{settings.widget.url}}" ng-attr-target="{{(settings.widget.tab == 'newtab')?'_blank':''}}">
-										<button class="yeloni-yn-yesbutton"><span class="yeloni-superlarge" editable>Yes</span><br/><span editable>I Love Customers</span></button>
+										<button class="yeloni-yn-yesbutton">
+											<!--<span class="yeloni-superlarge" editable>Yes</span><br/>-->
+											<span editable>I Love Customers</span>
+										</button>
 									</a>
 								</td>
 								<td valign="top" width="50%">										
-									<button class="yeloni-yn-nobutton"><span class="yeloni-superlarge" editable>No</span><br/><span editable>I don't like them</span></button>
+									<button class="yeloni-yn-nobutton">
+										<!--<span class="yeloni-superlarge" editable>No</span><br/>-->
+										<span editable>I don't like them</span>
+									</button>
 								</td>
 							</tr>
 						</table>
@@ -45,8 +50,6 @@
 					</div>
 
 				</div>
-
-			</div><!-- end yeloni-ynlayout -->
 
 		</div>
 		<div style="clear:both"></div>
